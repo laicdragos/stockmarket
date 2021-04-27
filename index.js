8acbf73092bf35fdcb961c20d148e76dfcc167d3
Laic Dragos <laicdragos@gmail.com>
2021-04-27 22:42:44 +0300

    getting information from search, updated api info

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const request = require('request');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 5000;
 //Majoritatea clodurilor prefera sa foloseasca porturile lor de aia am pus sau.
+
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
 
 //Set handlebars middleware
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set handlebar routes.
+//Set handlebar index GET route.
 app.get('/', function (req, res) {
     call_api( function(doneAPI) {
         res.render('home',{
@@ -21,15 +27,27 @@ app.get('/', function (req, res) {
         });
     });
 });
+
+//Set handlebar index POST route.
+app.post('/', function (req, res) {
+    call_api( function(doneAPI) {
+        //posted_stuff = req.body.stock_ticker;
+        res.render('home',{
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
+});
+
+
 //create route to about 
 app.get('/about.html', function (req, res) {
     res.render('about');
 });
 
 //create call_api function
-function call_api(finishedAPI){
+function call_api(finishedAPI, ticker){
 //API KEY pk_a76f8e36e6654855a196bd559508743a
-request('https://cloud.iexapis.com/stable/stock/gme/quote?token=pk_a76f8e36e6654855a196bd559508743a',{json:true},(err, res,body)=>{
+  request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_a76f8e36e6654855a196bd559508743a',{json:true},(err, res,body)=>{
   if(err) {return console.log(err);}
   if(res.statusCode === 200)
   finishedAPI(body);
