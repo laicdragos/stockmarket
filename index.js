b374b7c608a45494239f88a8b3ad1e360df8693d
Laic Dragos <laicdragos@gmail.com>
2021-04-28 14:08:31 +0300

    updated css, updated home/stocks/about section, footer

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
-//Majoritatea clodurilor prefera sa foloseasca porturile lor de aia am pus sau.
+//Majoritatea cloudurilor prefera sa foloseasca porturile lor de aia am pus sau.
 
 
 // use body parser middleware
@@ -20,24 +20,29 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 //Set handlebar index GET route.
-app.get('/', function (req, res) {
+app.get('/stocks.html', function (req, res) {
     call_api( function(doneAPI) {
-        res.render('home',{
+        res.render('stocks',{
             stock: doneAPI
         });
-    },'swn');
+    },'GOOG');
 });
 
 //Set handlebar index POST route.
 app.post('/', function (req, res) {
     call_api( function(doneAPI) {
         //posted_stuff = req.body.stock_ticker;
-        res.render('home',{
+        res.render('stocks',{
             stock: doneAPI,
         });
     }, req.body.stock_ticker);
 });
 
+
+//create route to Home 
+app.get('/home.html', function (req, res) {
+    res.render('home');
+});
 
 //create route to about 
 app.get('/about.html', function (req, res) {
